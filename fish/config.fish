6c13d8dd9c8d00ff08b8fed482d7affedd1b9468
Dustybluelight <a.wilkie2003@gmail.com>
2022-08-28 00:21:50 +0800

    chore: removed uneeded lines

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
-set -g fish_prompt_pwd_dir_length 0
-
 fish_vi_key_bindings
 
